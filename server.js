da1586e3276c1db142e76cf10c55b6cbef1ddca2
ArsenalVault <vikhyatjagawat007@gmail.com>
2020-07-19 10:20:48 +0530

    Face dectection back-end

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const register = require("./controlers/register");
+const signin = require("./controlers/signin");
+const profile = require("./controlers/profile");
+const image = require("./controlers/image.js");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "Alexm@son13",
+    database: "smart-brain",
+  },
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+// // Load hash from your password DB.
+
+// bcrypt.compare("veggies", hash, function(err, res) {
+//   // res = false
+// });
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
+
+/*
+
+/--> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userid --> GET = user
+/image --> PUT --> user
+
+*/
